135cc442e37713529d4af81dfe08fd7c1c6faf92
Herson Salinas <hersonhn@gmail.com>
2017-10-05 15:46:11 -0600

    livereload and uglify

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ thank mr skeltal
 
 ![](https://i.imgur.com/hoDrij8.gif)
 
-Mr. Skeltal is a skeleton for ES2015 applications using `gulp` and `webpack`, it also bundles `sass` for the stylesheets and `browsersync` for live reload.
+Mr. Skeltal is a skeleton for ES2015 applications using `gulp` and `webpack`, it also bundles `sass` for the stylesheets and `livereload` for, you know, live reload.
 
 It doesn't include configurations for html templates or live server, because is meant to be used with any backend platform (express, django, wordpress), so it only handles the compilation for scripts assets.
 
@@ -13,7 +13,7 @@ It doesn't include configurations for html templates or live server, because is 
 - `gulp css` compiles just the css.
 - `gulp js` compiles just the javascript.
 - `gulp watch` doesn't compile at the start, but keeps looking for changes.
-- `gulp prod` compiles and minify the css and javascript without browsersync integration.
+- `gulp prod` compiles and minify the css and javascript.
 
 - - -
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
+'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const plumber = require('gulp-plumber');
-const uglify  = require('gulp-uglify');
 const changed = require('gulp-changed');
+const composer  = require('gulp-uglify/composer');
 const gulpWebpack = require('gulp-webpack');
-const webpack = require('webpack');
+const livereload = require('gulp-livereload');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
 const named = require('vinyl-named');
+const uglifyES = require('uglify-es');
+const webpack = require('webpack');
+
+
+const conf = require('./conf.json');
+const minify = composer(uglifyES, console);
+
+
+function js() {
+  return gulp.src(['./src/js/*.js'])
+    .pipe(named())
+    .pipe(gulpWebpack({ output: { filename: '[name].js' } }, webpack));
+}
 
 
 gulp.task('js', function() {
-  return gulp.src(['./src/js/*.js'])
-    .pipe(named())
-    .pipe(gulpWebpack({ output: { filename: '[name].js' } }, webpack))
-    .pipe(gulp.dest('./build/js'));
+  return js()
+    .pipe(gulp.dest('./build/js'))
+    .pipe(livereload());
+});
+
+
+gulp.task('js:prod', function() {
+  return js()
+    .pipe(minify())
+    .pipe(gulp.dest('./build/js'))
 });
 
 
@@ -28,22 +48,28 @@ gulp.task('css', function() {
       includePaths: ['./node_modules']
     }))
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(livereload());
 });
 
 
 gulp.task('watch:css', function() {
+  livereload.listen(conf.livereload);
+
   gulp.watch('./src/scss/**/*.scss', ['css']);
   gulp.watch('./src/scss/*.scss', ['css']);
 });
 
 
 gulp.task('watch:js', function() {
+  livereload.listen(conf.livereload);
+
   gulp.watch('./src/js/**/*.js', ['js']);
   gulp.watch('./src/js/*.js', ['js']);
 });
 
 
 gulp.task('watch', ['watch:css', 'watch:js']);
+gulp.task('prod', ['css', 'js:prod']);
 gulp.task('default', ['css', 'js', 'watch']);
 
